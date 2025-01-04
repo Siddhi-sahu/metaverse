ef119a9d81ac069a525b77c7a22c9fa697695bf6
Siddhi Sahu <himanisahu739@gmail.com>
2025-01-04 17:52:38 +0530

    commit

--- metaverse/apps/http/src/routes/v1/space.ts
+++ metaverse/apps/http/src/routes/v1/space.ts
@@ -1,8 +1,68 @@
 import { Router } from "express";
+import { CreateSpaceSchmea } from "../../types";
+import client from "@repo/db/client";
+import { userMiddleware } from "../../middleware/user";
 
 export const spaceRouter = Router();
 
-spaceRouter.post("/", (req, res) => {
+spaceRouter.post("/", userMiddleware, async (req, res) => {
+    const parsedData = CreateSpaceSchmea.safeParse(req.body);
+    if (!parsedData.success) {
+        res.status(400).json({
+            msg: "Validation failed"
+        });
+        return;
+    }
+
+    if (!parsedData.data.mapId) {
+
+        const space = await client.space.create({
+            data: {
+                name: parsedData.data.name,
+                width: parseInt(parsedData.data.dimensions.split("x")[0]),
+                height: parseInt(parsedData.data.dimensions.split("x")[1]),
+                createrId: req.userId!
+            }
+
+        });
+        res.json({ spaceId: space.id });
+    }
+
+    const map = await client.map.findUnique({
+        where: {
+            id: parsedData.data.mapId
+        },
+        select: {
+            mapElements: true
+        }
+    });
+
+    if (!map) {
+        res.status(400).json({ msg: "Map not found!" });
+        return;
+    }
+
+    await client.$transaction(async () => {
+        const space = await client.space.create({
+            data: {
+                name: parsedData.data.name,
+                width: parseInt(parsedData.data.dimensions.split("x")[0]),
+                height: parseInt(parsedData.data.dimensions.split("x")[1]),
+                createrId: req.userId!
+            }
+        })
+
+        await client.spaceElements.createMany({
+            data: map.mapElements.map(e => ({
+                spaceId: space.id,
+                elementId: e.elementId,
+                x: e.x!,
+                y: e.y!
+            }))
+        })
+    })
+
+
 
 })
 spaceRouter.delete("/:spaceId", (req, res) => {
